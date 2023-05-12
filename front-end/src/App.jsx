8caaa12f1743ddf223addc6df240e8d74e968725
dRibSouza <92061792+dRibSouza@users.noreply.github.com>
2023-05-12 16:13:33 +0000

    front-end inicio

---
+++ front-end/src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css';
+import { AppRoutes } from './routes';
+
+
+export const App= () => {
+  return (
+    // Para fazer um header fixo para todos as páginas, podemos colocar ele aqui.<br/> // correção ele vai na página de rotas ao invés daqui :[
+    
+    <AppRoutes/>
+  );
+}
+
+
