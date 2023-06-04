dacf86d4ae87b4b42c37b6a533705a5b0dfe705c
Moisés Verissimo de Souza <102416201+VerissimoMoises@users.noreply.github.com>
2023-06-04 15:18:57 +0000

    commit upload dos arquivos

---
+++ README.md
@@ -0,0 +1,2 @@
+# DSM-G04-PI3-2023-1
+Repositório do GRUPO 4 - Projeto Interdisciplinar 3, 3º DSM Fatec Franca 2023/ 1. Alunos: Danilo Ribeiro de Souza, Matheus Oliveira Mancio, Moisés Veríssimo de Souza, Thayná de Souza Silva.
---
+++ back-end/controllers/patient.js
@@ -0,0 +1,98 @@
+const Patient = require('../models/Patient')
+
+const controller = {}   
+
+controller.create = async (req, res) =>{
+    try{
+        //Manda as informações que vieram em req.body
+        //para serem gravadas no banco de dados
+        await Patient.create(req.body)
+
+        //HTTP 201: Created
+        res.status(201).end()
+    }
+    catch(error){
+        console.error(error)
+        //HTTP 500: Internal Server Error
+        res.status(500).send(error)
+    }
+} 
+
+controller.retrieveAll = async (req, res) => {
+    try{
+        //Retorna todos os documentos da coleção
+        const result = await Patient.find()
+        //HTTP 200: OK (implícito)
+        res.send(result)
+    }
+    catch(error){
+        console.error(error)
+        //HTTP 500: Internal Server Error
+        res.status(500).send(error)
+    }
+}
+
+controller.retrieveOne = async(req, res) => {
+    try{
+        const result = await Patient.findById(req.params.id)
+
+        if(result){
+            //Encontrou o documento ~> HTTP 200: OK (implícito)
+            res.send(result)
+        }
+        else{
+            //Não encontrou o documento ~> HTTP 404: Not found
+            res.status(404).end()
+        }        
+    }
+    catch(error){
+        console.error(error)
+        //HTTP 500: Internal Server Error
+        res.status(500).send(error)
+    }
+}
+
+controller.update = async (req, res) => {
+    try{
+        const result = await Patient.findByIdAndUpdate(req.params.id, req.body)
+
+        if(result){
+            //Encontrou e atualizou ~> HTTP 204: No content
+            res.status(204).end()
+        }
+        else{
+            //Não encontrou para atualizar ~> HTTP 404: Not found
+            res.status(404).end()
+        }
+
+    }
+    catch(error){
+        console.error(error)
+        //HTTP 500: Internal Server Error
+        res.status(500).send(error)
+    }
+}
+
+controller.delete = async (req, res) => {
+    try{
+        const result = await Patient.findByIdAndDelete(req.params.id, req.body)
+
+        if(result){
+            //Encontrou e excluiu ~> HTTP 204: No content
+            res.status(204).end()
+        }
+        else{
+            //Não encontrou para excluir ~> HTTP 404: Not found
+            res.status(404).end()
+        }
+
+    }
+    catch(error){
+        console.error(error)
+        //HTTP 500: Internal Server Error
+        res.status(500).send(error)
+    }
+}
+
+
+module.exports = controller
